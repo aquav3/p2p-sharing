6bb7d076d1d8e770aaf716b9773408f2c0a5d555
aquav3 <kyrbynyt@gmail.com>
2023-07-28 12:29:26 +0300

    Added simple control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::{TcpStream, TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to p2p-sharing");
+    println!("Do you want to either connect to a port or create one?");
+    println!("1: Connect");
+    println!("2: Create");
+
+    let mut option: String = String::new();
+    std::io::stdin().read_line(&mut option).unwrap();
+    option = option.trim().to_string();
+
+    match option.as_str() {
+        "1" => {}
+        "2" => {}
+        _ => {
+            println!("Invalid option specified!)")
+        }
+    }
 }
